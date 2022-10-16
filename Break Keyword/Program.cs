3cafdd3c9b3c733eb3d59df2aeb45cba77b6feca
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 14:50:32 +0600

    Add project files.

---
+++ Break Keyword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            for (; ; )
+            {
+                Console.WriteLine($"{++i}");
+                if (i == 10)
+                {
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
